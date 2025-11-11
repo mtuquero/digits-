946aa5841ab386e78664a9a9ef4d92611c23633d
Mariah Tuquero <mariah27@hawaii.edu>
2025-11-10 23:46:36 -1000

    Clean Up

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -42,13 +42,13 @@ const SignIn = () => {
                     <input name="password" type="password" className="form-control" />
                   </Form.Group>
                   <Button type="submit" className="mt-3">
-                    Signin
+                    Sign in
                   </Button>
                 </Form>
               </Card.Body>
               <Card.Footer>
                 Don&apos;t have an account?
-                <a href="/auth/signup">Sign up</a>
+                <a href="/auth/sign up">Sign up</a>
               </Card.Footer>
             </Card>
           </Col>
